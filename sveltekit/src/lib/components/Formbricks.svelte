a8a6e50788351c2403c68d32c68577df80878b7a
Midka <me@midka.dev>
2023-09-01 11:26:18 +0000

    feat(sveltekit): initial commit

---
+++ sveltekit/src/lib/components/Formbricks.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { navigating } from '$app/stores';
+  import type fbT from '@formbricks/js';
+  import { onMount } from 'svelte';
+
+  let formbricks: typeof fbT;
+
+  onMount(() => {
+    import('@formbricks/js').then((fb) => {
+      formbricks = fb.default;
+      formbricks.init({
+        environmentId: 'clg6riezp0005lq0hzjimosc7',
+        apiHost: 'https://app.formbricks.com',
+        debug: true,
+      });
+
+      (window as any).formbricks = formbricks;
+    });
+  });
+
+  $: if ($navigating) formbricks.registerRouteChange();
+</script>
